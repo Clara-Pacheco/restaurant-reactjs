bb398a8ee7f06f4c9422072614ba0084599d8747
Clara <clarapachecodev@gmail.com>
2024-03-12 11:02:14 -0300

    Import all the components to App.jsx and start structuring them inside the App.jsx arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { FooterOverlay } from "./components/Footer/FooterOverlay";
-import { Newsletter } from "./components/Footer/Newsletter";
-import { MenuItem } from "./components/MenuItem/MenuItem";
+// import { FooterOverlay } from "./components/Footer/FooterOverlay";
+// import { Newsletter } from "./components/Footer/Newsletter";
+// import { MenuItem } from "./components/MenuItem/MenuItem";
 import { NavBar } from "./components/Navbar/NavBar";
-import { SubHeading } from "./components/SubHeading/SubHeading";
+// import { SubHeading } from "./components/SubHeading/SubHeading";
+
+import { AboutUs } from "./containers/AboutUs/AboutUs";
+import { Chef } from "./containers/Chef/Chef";
+import { FindUs } from "./containers/Findus/FindUs";
+import { Footer } from "./containers/Footer/Footer";
+import { Gallery } from "./containers/Gallery/Gallery";
+import { Header } from "./containers/Header/Header";
+import { Intro } from "./containers/Intro/Intro";
+import { Laurels } from "./containers/Laurels/Laurels";
+import { Menu } from "./containers/Menu/Menu";
 
 export const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
       <NavBar />
-      <SubHeading />
-      <MenuItem />
-      <Newsletter />
-      <FooterOverlay />
+      <Header />
+      <AboutUs />
+      <Menu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   );
 };
