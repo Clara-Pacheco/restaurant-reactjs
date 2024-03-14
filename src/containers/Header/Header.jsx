b67aa873cc124e994c6317fe72027221120f1ce8
Clara <clarapachecodev@gmail.com>
2024-03-14 11:00:21 -0300

    Build `Header` component passing the value for the props `title`

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,5 +1,17 @@
 import "./Header.css";
 
+// import { images } from "../../constants/images";
+
+import { SubHeading } from "../../components/SubHeading/SubHeading";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__warpper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new Flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      </div>
+      <div className="app__wrapper_img"></div>
+    </div>
+  );
 };
