e1bcd0962b2d6c88deb9acaf40e30d86ada06e49
Clara <clarapachecodev@gmail.com>
2024-03-14 10:58:18 -0300

    Comment components and importation that were not created yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@
 import { NavBar } from "./components/Navbar/NavBar";
 // import { SubHeading } from "./components/SubHeading/SubHeading";
 
-import { AboutUs } from "./containers/AboutUs/AboutUs";
-import { Chef } from "./containers/Chef/Chef";
-import { FindUs } from "./containers/Findus/FindUs";
-import { Footer } from "./containers/Footer/Footer";
-import { Gallery } from "./containers/Gallery/Gallery";
+// import { AboutUs } from "./containers/AboutUs/AboutUs";
+// import { Chef } from "./containers/Chef/Chef";
+// import { FindUs } from "./containers/Findus/FindUs";
+// import { Footer } from "./containers/Footer/Footer";
+// import { Gallery } from "./containers/Gallery/Gallery";
 import { Header } from "./containers/Header/Header";
-import { Intro } from "./containers/Intro/Intro";
-import { Laurels } from "./containers/Laurels/Laurels";
-import { Menu } from "./containers/Menu/Menu";
+// import { Intro } from "./containers/Intro/Intro";
+// import { Laurels } from "./containers/Laurels/Laurels";
+// import { Menu } from "./containers/Menu/Menu";
 
 export const App = () => {
   return (
     <div>
       <NavBar />
       <Header />
-      <AboutUs />
+      {/* <AboutUs />
       <Menu />
       <Chef />
       <Intro />
       <Laurels />
       <Gallery />
       <FindUs />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
