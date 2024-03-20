36d662422106e6395d97c3d82f8d3c90fc6f4cfc
Clara <clarapachecodev@gmail.com>
2024-03-20 10:23:26 -0300

    Create a state to control when the overlay is shown and when its not

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -7,6 +7,7 @@ import { meal } from "../../constants";
 
 export const Intro = () => {
   const [playVideo, setPlayVideo] = React.useState(false);
+  const [showOverlay, setShowOverlay] = React.useState(true);
   const vidRef = React.useRef();
 
   function handleVideo() {
@@ -28,19 +29,23 @@ export const Intro = () => {
         loop
         controls={false}
         muted
+        onPlay={() => setShowOverlay(false)}
+        onClick={() => setShowOverlay(true)}
       />
-      <div className="app__video-overlay flex__center">
-        <div
-          className="app__video-overlay_circle flex__center"
-          onClick={handleVideo}
-        >
-          {playVideo ? (
-            <BsPauseFill color="#fff" fontSize={30} />
-          ) : (
-            <BsFillPlayFill color="#fff" fontSize={30} />
-          )}
+      {showOverlay && (
+        <div className="app__video-overlay flex__center">
+          <div
+            className="app__video-overlay_circle flex__center"
+            onClick={handleVideo}
+          >
+            {playVideo ? (
+              <BsPauseFill color="#fff" fontSize={30} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={30} />
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
